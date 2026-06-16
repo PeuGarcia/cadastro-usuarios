e9823d849a082616a9fca780a89c794556900a8e
Pedro Garcia <pedrogarcia4433@gmail.com>
2026-06-16 14:57:19 -0300

    Realiza a validação de CPF

--- script.js
+++ script.js
@@ -105,6 +105,36 @@ formulario.addEventListener("submit", function(event) {
         return;
     }
 
+    function ValidarCPF(cpf) {
+    cpf = cpf.replace(/\D/g, "");
+    if (/^(\d)\1{10}$/.test(cpf)) return false;
+        var soma;
+        var resto;
+        soma = 0;
+    if (cpf == "00000000000") return false;
+
+    for (i=1; i<=9; i++) soma = soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+    resto = (soma * 10) % 11;
+
+        if ((resto == 10) || (resto == 11))  resto = 0;
+        if (resto != parseInt(cpf.substring(9, 10)) ) return false;
+
+    soma = 0;
+        for (i = 1; i <= 10; i++) soma = soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+        resto = (soma * 10) % 11;
+
+        if ((resto == 10) || (resto == 11)) resto = 0;
+        if (resto != parseInt(cpf.substring(10,11))) return false;
+        return true;
+    }
+
+        var cpfValido = ValidarCPF(usuario.cpf);
+        if (!cpfValido) {
+        alert("CPF inválido! Por favor, digite um CPF válido.");
+        return;
+    }
+
+
     // Recupera usuários já cadastrados.
     const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
